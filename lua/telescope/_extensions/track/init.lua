b1169881a4a696a5298617022076e8ef7fff77a9
dharmx <dharmx.dev@gmail.com>
2023-03-23 22:44:56 +0530

    chore: added position marks
    Signed-off-by: dharmx <dharmx.dev@gmail.com>

--- lua/telescope/_extensions/track/init.lua
+++ lua/telescope/_extensions/track/init.lua
@@ -5,17 +5,74 @@ if not present then
   return
 end
 
-local interface = require("track.interface")
+local A = vim.api
 local mark = require("track.mark")
 local config = require("track.config")
+local current_config = config.get()
+local util = require("track.util")
+
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local actions = require("telescope.actions")
+local state = require("telescope.actions.state")
+local values = require("telescope.config").values
+
+local function marks(options)
+  config.merge(options)
+  if not mark._loaded then mark.load() end
+  current_config.callbacks.on_open()
+
+  -- TODO: Implement a previewer for viewing metadata and file contents.
+  local picker = pickers.new(current_config, {
+    prompt_title = "Track",
+    finder = finders.new_table({
+      results = vim.tbl_keys(mark.marks()),
+      -- TODO: Implement entry_makers.
+    }),
+    sorter = values.file_sorter(current_config),
+    attach_mappings = function(buffer, map)
+      local current = state.get_current_picker(buffer)
+      -- TODO: Implement copying and pasting (both mutiple and single).
+      -- TODO: Implement move up and move down (both multiple and single).
+      -- TODO: Implement highlight_one_row.
+      -- TODO: Implement row edits.
+      -- TODO: Implement save_modify.
+
+      map("n", "dd", function()
+        current_config.callbacks.on_delete(buffer, current)
+        current:delete_selection(util.mute)
+      end)
+      map("i", "<C-D>", function()
+        current_config.callbacks.on_delete(buffer, current)
+        current:delete_selection(util.mute)
+      end)
+
+      actions.close:replace(function()
+        local window = current.original_win_id
+        local valid, cursor = pcall(A.nvim_win_get_cursor, window)
+        actions.close_pum(buffer)
+        pickers.on_close_prompt(buffer)
+        pcall(A.nvim_set_current_win, window)
+        if valid and A.nvim_get_mode().mode == "i" and current._original_mode ~= "i" then
+          pcall(A.nvim_win_set_cursor, window, { cursor[1], cursor[2] + 1 })
+        end
+        if current_config.save.on_close then mark.save() end
+        current_config.callbacks.on_close(buffer, current)
+      end)
+
+      actions.select_default:replace(function()
+        actions.close(buffer)
+        current_config.callbacks.on_choose(buffer, current)
+      end)
+      return true
+    end,
+  })
+  picker:find()
+end
 
 return telescope.register_extension({
   setup = config.merge,
   exports = {
-    marks = function(options)
-      config.merge(options)
-      if not mark._loaded then mark.load() end
-      interface.view()
-    end,
+    marks = marks
   },
 })
--- plugin/track.lua
+++ plugin/track.lua
@@ -7,12 +7,16 @@ if vim.g.loaded_track == 1 then return end
 vim.g.loaded_track = 1
 local cmd = vim.api.nvim_create_user_command
 
+-- TODO: Implement bang, range, repeat, motions and bar.
+
 cmd("Track", function(...)
   local option = (...).args
+  local mark = require("track.mark")
+
   if option == "save" then
-    require("track.mark").save(function() vim.notify("Saved.") end)
+    mark.save(function() vim.notify("Saved.") end)
   elseif option == "reload" then
-    require("track.mark").load(function() vim.notify("Reloaded.") end)
+    mark.load(function() vim.notify("Reloaded.") end)
   else require("telescope").extensions.track.marks() end
 end, {
   desc = "Main controls.",
@@ -22,11 +26,13 @@ end, {
   end
 })
 
-cmd("Mark", function(...)
+cmd("MarkFile", function(...)
   local files = (...).fargs
+  local mark = require("track.mark")
+
   if #files == 0 then table.insert(files, vim.fn.expand("%")) end
   for _, file in ipairs(files) do
-    require("track.mark").mark(file)
+    mark.mark_file(file)
   end
 end, {
   desc = "Mark current files(s). This can also takes in a file path.",
@@ -34,16 +40,59 @@ end, {
   complete = "file",
 })
 
-cmd("Unmark", function(...)
+cmd("UnmarkFile", function(...)
   local files = (...).fargs
+  local mark = require("track.mark")
+
   if #files == 0 then table.insert(files, vim.fn.expand("%")) end
   for _, file in ipairs(files) do
-    require("track.mark").unmark(file)
+    mark.unmark_file(file)
   end
 end, {
-  desc = "Mark current files(s). This can also takes in a file path.",
+  desc = "Unmark current files(s). This can also takes in a file path.",
   nargs = "*",
   complete = function()
     return vim.tbl_keys(require("track.mark").marks())
   end,
+})
+
+cmd("MarkPosition", function(...)
+  local args = (...).fargs
+  local mark = require("track.mark")
+
+  local file = vim.fn.expand("%")
+  if vim.tbl_isempty(args) then
+    mark.mark_position(file, vim.api.nvim_win_get_cursor(0))
+  else
+    assert(#args % 2 == 0)
+    for index = 1, #args, 2 do
+      mark.mark_position(file, {
+        tonumber(args[index]), -- row
+        tonumber(args[index + 1]), -- column
+      })
+    end
+  end
+end, {
+  nargs = "*",
+  complete = "file",
+})
+
+cmd("UnmarkPosition", function(...)
+  local args = (...).fargs
+  local mark = require("track.mark")
+  local file = vim.fn.expand("%")
+
+  if vim.tbl_isempty(args) then
+    mark.unmark_position(file, vim.api.nvim_win_get_cursor(0))
+  else
+    assert(#args % 2 == 0)
+    for index = 1, #args, 2 do
+      mark.unmark_position(file, {
+        tonumber(args[index]), -- row
+        tonumber(args[index + 1]), -- column
+      })
+    end
+  end
+end, {
+  nargs = "*",
 })
