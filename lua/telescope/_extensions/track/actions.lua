943abb83a9472a0ab618a1feb6feee051e91a281
dharmx <dharmx.dev@gmail.com>
2023-03-29 01:27:15 +0530

    chore: removed the usage of vim.NIL
    Signed-off-by: dharmx <dharmx.dev@gmail.com>

--- lua/telescope/_extensions/track/actions.lua
+++ lua/telescope/_extensions/track/actions.lua
@@ -2,14 +2,15 @@ local M = setmetatable({}, {
   __index = function(_, key) error("Key does not exist for actions: " .. tostring(key)) end,
 })
 
-local util = require("track.util")
-local action_state = require("telescope.actions.state")
+local Util = require("track.util")
+local state = require("telescope.state")
+local actions_state = require("telescope.actions.state")
 local mt = require("telescope.actions.mt")
 
 function M.delete(buffer)
-  local current_picker = action_state.get_current_picker(buffer)
+  local current_picker = actions_state.get_current_picker(buffer)
   local entries = vim.F.if_nil(current_picker:get_multi_selection(), {})
-  current_picker:delete_selection(util.mute)
+  current_picker:delete_selection(Util.mute)
   if #entries == 0 then table.insert(entries, current_picker:get_selection()) end
 end
 
