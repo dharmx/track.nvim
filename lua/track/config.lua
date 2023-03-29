feb315ecdf1ec09ad1555c527a793964b3285093
dharmx <dharmx.dev@gmail.com>
2023-03-29 22:25:22 +0530

    feat: added entrymaker for views picker
    Signed-off-by: dharmx <dharmx.dev@gmail.com>

--- lua/track/config.lua
+++ lua/track/config.lua
@@ -1,20 +1,16 @@
 local M = {}
 
 -- WARN: Do not import track.config in track.util.
-local util = require("track.util")
+local Util = require("track.util")
 
 -- TODO: Implement validation (vim.validate) and config fallback.
 -- TODO: Defaults (M.defaults) will be used if config values are invalid.
 -- TODO: Implement a way to show hidden files.
 -- TODO: Asyncify saving.
--- TODO: disable_devicons = boolean
 -- TODO: Add view = "metadata"|"contents"
 
 M._defaults = {
   savepath = "/tmp/track.json",
-  prompt_prefix = " 粒 ",
-  previewer = false,
-  initial_mode = "insert",
   save = {
     on_views_close = true,
     on_marks_close = false,
@@ -22,80 +18,164 @@ M._defaults = {
     on_roots_close = false,
     on_track_close = false,
   },
-  layout_config = {
-    preview_cutoff = 1,
-    width = function(_, max_columns, _) return math.min(max_columns, 50) end,
-    height = function(_, _, max_lines) return math.min(max_lines, 15) end,
-  },
-  hooks = {
-    on_track_close = util.mute,
-    on_views_close = util.mute,
-    on_roots_close = util.mute,
-    on_bundles_close = util.mute,
-    on_marks_close = util.mute,
+  pickers = {
+    views = {
+      prompt_prefix = " 粒 ",
+      previewer = false,
+      initial_mode = "insert",
+      layout_config = {
+        preview_cutoff = 1,
+        width = function(_, max_columns, _) return math.min(max_columns, 50) end,
+        height = function(_, _, max_lines) return math.min(max_lines, 15) end,
+      },
+      hooks = {
+        on_close = Util.mute,
+        on_open = Util.mute,
+        on_each_view = function(buffer, map, mark)
+          local keys = Util.serial_keymap(mark.index)
+          local function command()
+            require("telescope.actions").close(buffer)
+            Util.open_file(mark.path)
+          end
+          map("n", keys.normal, command)
+          map("i", keys.insert, command)
+        end,
+        on_choose = function(_, picker)
+          local entries = vim.F.if_nil(picker:get_multi_selection(), {})
+          if #entries == 0 then table.insert(entries, picker:get_selection()) end
+          vim.tbl_map(function(entry) Util.open_file(entry.value.path) end, entries)
+        end,
+      },
+      mappings = function(_, map)
+        local actions = require("telescope.actions")
+        map("i", "<C-K>", actions.move_selection_previous)
+        map("i", "<C-J>", actions.move_selection_next)
+        map("i", "<C-E>", actions.close)
+        map("n", "q", actions.close)
 
-    on_track_open = util.mute,
-    on_views_open = util.mute,
-    on_roots_open = util.mute,
-    on_bundles_open = util.mute,
-    on_marks_open = util.mute,
+        local Actions = require("telescope._extensions.track.actions")
+        map("n", "dd", Actions.delete_view)
+        map("i", "<C-D>", Actions.delete_view)
+      end,
+      track_options = {
+        bundle = "main",
+        root = vim.fn.getcwd,
+      },
+      disable_devicons = false,
+      icons = {
+        separator = " ",
+        accessible = " ",
+        inaccessible = " ",
+        focused = " ",
+        listed = "",
+      },
+      display_path = function(path)
+        return vim.fn.fnamemodify(path, ":t")
+      end,
+    },
+    marks = {
+      prompt_prefix = " 粒 ",
+      previewer = false,
+      initial_mode = "insert",
+      layout_config = {
+        preview_cutoff = 1,
+        width = function(_, max_columns, _) return math.min(max_columns, 50) end,
+        height = function(_, _, max_lines) return math.min(max_lines, 15) end,
+      },
+      hooks = {
+        on_close = Util.mute,
+        on_open = Util.mute,
+        on_choose = Util.mute,
+      },
+      mappings = function(_, map)
+        local track_actions = require("telescope._extensions.track.actions")
+        map("n", "dd", track_actions.delete)
+        map("i", "<C-D>", track_actions.delete)
 
-    on_views_choose = util.mute,
-    on_roots_choose = util.mute,
-    on_bundles_choose = util.mute,
-    on_track_choose = function(_, picker)
-      local entry = picker:get_selection()
-      require("telescope").extensions.track[entry.value:lower()]()
-    end,
-    on_marks_choose = function(_, picker)
-      local entries = vim.F.if_nil(picker:get_multi_selection(), {})
-      if #entries == 0 then table.insert(entries, picker:get_selection()) end
-      vim.tbl_map(function(entry) vim.cmd("confirm edit " .. entry.value) end, entries)
-    end,
-  },
-  mappings = {
-    track = function(_, map)
-      local actions = require("telescope.actions")
-      map("i", "<C-K>", actions.move_selection_previous)
-      map("i", "<C-J>", actions.move_selection_next)
-      map("i", "<C-E>", actions.close)
-    end,
-    roots = function(_, map)
-      local actions = require("telescope.actions")
-      map("i", "<C-K>", actions.move_selection_previous)
-      map("i", "<C-J>", actions.move_selection_next)
-      map("i", "<C-E>", actions.close)
-    end,
-    views = function(_, map)
-      local actions = require("telescope.actions")
-      map("i", "<C-K>", actions.move_selection_previous)
-      map("i", "<C-J>", actions.move_selection_next)
-      map("i", "<C-E>", actions.close)
-    end,
-    marks = function(_, map)
-      local track_actions = require("telescope._extensions.track.actions")
-      map("n", "dd", track_actions.delete)
-      map("i", "<C-D>", track_actions.delete)
+        local actions = require("telescope.actions")
+        map("i", "<C-K>", actions.move_selection_previous)
+        map("i", "<C-J>", actions.move_selection_next)
+        map("i", "<C-S>", actions.file_split)
+        map("i", "<C-V>", actions.file_vsplit)
+        map("i", "<C-E>", actions.close)
+      end,
+      disable_devicons = false,
+    },
+    bundles = {
+      prompt_prefix = " 粒 ",
+      previewer = false,
+      initial_mode = "insert",
+      layout_config = {
+        preview_cutoff = 1,
+        width = function(_, max_columns, _) return math.min(max_columns, 50) end,
+        height = function(_, _, max_lines) return math.min(max_lines, 15) end,
+      },
+      hooks = {
+        on_close = Util.mute,
+        on_open = Util.mute,
+        on_choose = Util.mute,
+      },
+      mappings = function(_, map)
+        local track_actions = require("telescope._extensions.track.actions")
+        map("n", "dd", track_actions.delete)
+        map("i", "<C-D>", track_actions.delete)
 
-      local actions = require("telescope.actions")
-      map("i", "<C-K>", actions.move_selection_previous)
-      map("i", "<C-J>", actions.move_selection_next)
-      map("i", "<C-S>", actions.file_split)
-      map("i", "<C-V>", actions.file_vsplit)
-      map("i", "<C-E>", actions.close)
-    end,
-    bundles = function(_, map)
-      local track_actions = require("telescope._extensions.track.actions")
-      map("n", "dd", track_actions.delete)
-      map("i", "<C-D>", track_actions.delete)
-
-      local actions = require("telescope.actions")
-      map("i", "<C-K>", actions.move_selection_previous)
-      map("i", "<C-J>", actions.move_selection_next)
-      map("i", "<C-S>", actions.file_split)
-      map("i", "<C-V>", actions.file_vsplit)
-      map("i", "<C-E>", actions.close)
-    end,
+        local actions = require("telescope.actions")
+        map("i", "<C-K>", actions.move_selection_previous)
+        map("i", "<C-J>", actions.move_selection_next)
+        map("i", "<C-S>", actions.file_split)
+        map("i", "<C-V>", actions.file_vsplit)
+        map("i", "<C-E>", actions.close)
+      end,
+      disable_devicons = false,
+    },
+    roots = {
+      prompt_prefix = " 粒 ",
+      previewer = false,
+      initial_mode = "insert",
+      layout_config = {
+        preview_cutoff = 1,
+        width = function(_, max_columns, _) return math.min(max_columns, 50) end,
+        height = function(_, _, max_lines) return math.min(max_lines, 15) end,
+      },
+      hooks = {
+        on_close = Util.mute,
+        on_open = Util.mute,
+        on_choose = Util.mute,
+      },
+      mappings = function(_, map)
+        local actions = require("telescope.actions")
+        map("i", "<C-K>", actions.move_selection_previous)
+        map("i", "<C-J>", actions.move_selection_next)
+        map("i", "<C-E>", actions.close)
+      end,
+      disable_devicons = false,
+    },
+    track = {
+      prompt_prefix = " 粒 ",
+      previewer = false,
+      initial_mode = "insert",
+      layout_config = {
+        preview_cutoff = 1,
+        width = function(_, max_columns, _) return math.min(max_columns, 50) end,
+        height = function(_, _, max_lines) return math.min(max_lines, 15) end,
+      },
+      hooks = {
+        on_close = Util.mute,
+        on_open = Util.mute,
+        on_choose = function(_, picker)
+          local entry = picker:get_selection()
+          require("telescope").extensions.track[entry.value:lower()]()
+        end,
+      },
+      mappings = function(_, map)
+        local actions = require("telescope.actions")
+        map("i", "<C-K>", actions.move_selection_previous)
+        map("i", "<C-J>", actions.move_selection_next)
+        map("i", "<C-E>", actions.close)
+      end,
+      disable_devicons = false,
+    },
   },
   log = {
     plugin = "track.nvim",
