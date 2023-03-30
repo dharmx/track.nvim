baffdcfc2b50b9f7c05e08944b4e3244c513315f
dharmx <dharmx.dev@gmail.com>
2023-03-31 04:37:31 +0530

    chore: path_display and history recording to views
    Signed-off-by: dharmx <dharmx.dev@gmail.com>

--- lua/telescope/_extensions/track/entry.lua
+++ lua/telescope/_extensions/track/entry.lua
@@ -1,48 +1,39 @@
 local M = {}
 
 local entry_display = require("telescope.pickers.entry_display")
-local strings = require("plenary.strings")
 local utils = require("telescope.utils")
 local make_entry = require("telescope.make_entry")
 
 function M.views(options)
   local icons = options.icons
   local disable_devicons = options.disable_devicons
-  local icon_width = 0
-  if not disable_devicons then
-    local icon, _ = utils.get_devicons("fname", disable_devicons)
-    icon_width = strings.strdisplaywidth(icon)
-  end
-
-  local display = entry_display.create({
+  local displayer = entry_display.create({
     separator = icons.separator,
     items = {
       { width = 2 },
       { width = 3 },
       { width = 2 },
-      { width = icon_width },
+      { width = 1 },
       { remaining = true },
     },
   })
 
   local function make_display(entry)
-    local icon, hl = utils.get_devicons(entry.value.path, disable_devicons)
     local marker, marker_hl = icons.accessible, "TrackViewsAccessible"
-
     if not entry.value:exists() then
       marker, marker_hl = icons.inaccessible, "TrackViewsInaccessible"
     end
-    local display_path, display_hl = entry.value.path, ""
-    if entry.value.name == entry.value.path then
+
+    options.cwd = options.track_options.root_path()
+    local display, display_hl = utils.transform_path(options, entry.value.path), ""
+    if entry.value.focused_path  == entry.value.absolute then
       display_hl = "TrackViewsFocusedDisplay"
       marker, marker_hl = icons.focused, "TrackViewsFocused"
     end
-    if options.display_path then
-      assert(type(options.display_path) == "function", "views_path should be false|nil|fun(path: string): string")
-      display_path = options.display_path(entry.value.path)
-    end
 
-    local listed, listed_hl = " ", ""
+    local icon, icon_hl = utils.get_devicons(entry.value.path, disable_devicons)
+    if not icon_hl then icon, icon_hl = icons.file, "TrackViewsFileIcon" end
+    local listed, listed_hl = icons.unlisted, "TrackViewsMarkUnlisted"
     for _, info in ipairs(vim.fn.getbufinfo({ loaded = 1 })) do
       if info.name == entry.value.absolute and info.listed == 1 then
         listed, listed_hl = icons.listed, "TrackViewsMarkListed"
@@ -50,17 +41,17 @@ function M.views(options)
       end
     end
 
-    return display({
+    return displayer({
       { entry.value.index, "TrackViewsIndex" },
       { marker, marker_hl },
       { listed, listed_hl },
-      { icon, hl },
-      { display_path, display_hl },
+      { icon, icon_hl },
+      { display, display_hl },
     })
   end
 
   return function(entry)
-    entry.name = vim.fn.bufname()
+    entry.focused_path = vim.fn.fnamemodify(vim.fn.bufname(), ":p")
     return make_entry.set_default_entry_mt({
       value = entry,
       ordinal = entry.index .. " : " .. entry.path,
