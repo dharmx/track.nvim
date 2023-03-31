2f7d8fc04870d58c4560c30d10e93092c4ebf868
dharmx <dharmx.dev@gmail.com>
2023-03-31 14:39:53 +0530

    chore: supports cwd

--- lua/track/containers/bundle.lua
+++ lua/track/containers/bundle.lua
@@ -9,76 +9,21 @@ function Bundle:new(fields)
   bundle.label = fields.label
   bundle.marks = vim.F.if_nil(fields.marks, {})
   bundle.views = vim.F.if_nil(fields.views, {})
-
-  -- TODO: Add option fields.disable_history.
-  -- TODO: Add option fields.max_history (Sliding Window Algorithm).
+  bundle.disable_history = vim.F.if_nil(fields.disable_history, true)
+  bundle.maximum_history  = vim.F.if_nil(fields.maximum_history , 10)
   bundle.history = vim.F.if_nil(fields.history, {})
   bundle._type = "mark"
 
-  setmetatable(bundle.marks, {
-    __call = function(marks, action)
-      if action == "string" then return vim.tbl_keys(marks) end
-      return vim.tbl_values(marks)
-    end,
-  })
-  setmetatable(bundle.views, {
-    __call = function(views, _)
-      local view_marks = {}
-      for _, view in ipairs(views) do
-        table.insert(view_marks, bundle.marks[view])
-      end
-      return view_marks
-    end,
-  })
-
   self.__index = self
   setmetatable(bundle, self)
+  self._callize_views(bundle)
+  self._callize_marks(bundle)
   return bundle
 end
 
-function Bundle:add_mark(path, label)
-  vim.validate({
-    path = { path, "string" },
-    label = { label, "string", true },
-  })
-  self.marks[path] = Mark:new({
-    path = path,
-    label = label,
-  })
-  table.insert(self.views, path)
-  return self.marks[path]
-end
-
-function Bundle:remove_mark(path)
-  assert(path and type(path), "path: string cannot be empty.")
-  local removed_mark = self.marks[path]
-  self.marks[path] = nil
-
-  -- NOTE: Is there truly no other way? Why must table.remove be so weird?
-  self.views = vim.tbl_filter(function(item) return item ~= path end, self.views)
-  setmetatable(self.views, {
-    __call = function(views, _)
-      local marks = {}
-      for _, view in ipairs(views) do
-        table.insert(marks, self.marks[view])
-      end
-      return marks
-    end,
-  })
-  table.insert(self.history, removed_mark)
-  return removed_mark
-end
-
-function Bundle:clear()
-  for _, mark in pairs(self.marks) do table.insert(self.history, mark) end
-  self.marks = {}
-  self.views = {}
-  setmetatable(self.marks, {
-    __call = function(marks, action)
-      if action == "string" then return vim.tbl_keys(marks) end
-      return vim.tbl_values(marks)
-    end,
-  })
+-- Metatable Setters {{{
+---@private
+function Bundle:_callize_views()
   setmetatable(self.views, {
     __call = function(views, _)
       local view_marks = {}
@@ -88,6 +33,55 @@ function Bundle:clear()
       return view_marks
     end,
   })
+end
+
+---@private
+function Bundle:_callize_marks()
+  setmetatable(self.marks, {
+    __call = function(marks, action)
+      if action == "string" then return vim.tbl_keys(marks) end
+      return vim.tbl_values(marks)
+    end,
+  })
+end
+-- }}}
+
+function Bundle:add_mark(mark, label)
+  if type(mark) == "table" and mark._type == "mark" then
+    self.marks[mark.path] = mark
+    return self.marks[mark.path]
+  end
+  self.marks[mark] = Mark:new({ path = mark, label = label })
+  table.insert(self.views, mark)
+  return self.marks[mark]
+end
+
+function Bundle:remove_mark(path)
+  assert(path and type(path), "path: string cannot be empty.")
+  local removed_mark = self.marks[path]
+  self.marks[path] = nil
+  self.views = vim.tbl_filter(function(item) return item ~= path end, self.views)
+  self:_callize_views()
+  self:insert_history(removed_mark)
+  return removed_mark
+end
+
+function Bundle:clear()
+  for _, mark in pairs(self.marks) do
+    table.insert(self.history, mark)
+  end
+  self.marks = {}
+  self.views = {}
+  self:_callize_views()
+  self:_callize_marks()
+end
+
+function Bundle:insert_history(mark, force)
+  local mark_type = type(mark)
+  assert(mark_type == "table" and mark._type == "mark", "mark: Mark cannot be nil.")
+  if self.disable_history and not force then return end
+  table.insert(self.history, 1, mark)
+  if #self.history > self.maximum_history then table.remove(self.history, #self.history) end
 end
 
 function Bundle:empty() return vim.tbl_isempty(self.marks) end
